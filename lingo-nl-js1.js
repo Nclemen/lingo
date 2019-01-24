c8e5e9ac8131446be6b7bdab335d55d040a4a832
Unknown <sonche17@hotmail.com>
2019-01-24 22:37:11 +0100

    .

--- lingo-nl-js1.js
+++ lingo-nl-js1.js
@@ -1,16 +1,9 @@
-var woordDIV  = document.getElementById('woordDIV');
-var gameDiv = document.createElement("div");
 var woord =words[randomWoord(words)]
-var antwoord = [];
+var antwoord;
+var kopiewoordSpl= [];
 var row     = 1;
-var rowbegin = 1
-
-///////////////////////////////////////////////////////
-
-
 var spelbord = document.getElementById('spelbord');
 var letters = document.getElementsByClassName('letters');
-var getal = 1;
 
 pagina();
 function pagina() {
@@ -39,57 +32,97 @@ function pagina() {
  document.body.appendChild(spelbord);
 }
 
-
-
-function wordSelect() {
-  var word = words[math.floor(math.random()*word.length)].toUpperCase();
-  document.write(word);
-}
-function randomWoord(items)
-{
+function randomWoord(items){
 return [Math.floor(Math.random()*items.length)];
 }
+
 function newWoord() {
   woord = words[randomWoord(words)];
+  console.log(woord);
 }
-gameDiv.setAttribute("id","gameDiv");
-console.log(woord)
 
+function kopieer() {
+  var randomwoord = woord.split("");
+  for (var i = 0; i < randomwoord.length; i++) {
+    kopiewoordSpl[i] = randomwoord[i];
+  }
+}
 
+kopieer();
+document.getElementById('opnieuw').style.display = "none";
+console.log(woord);
 
 var mainDIV   = document.getElementById('mainDIV');
 var inputDIV  = document.getElementById('inputDIV');
 var word      = document.getElementById('word');
 var theLetter = document.getElementById('raad');
 
-
-
-
 function check() {
   var splword = woord.split("");
   var splraad = theLetter.value.split("");
-  var check   = woord.includes(splraad[i]);
 
   if (theLetter === woord) {
     return gewonnen = true;
-  }
-  else {
+  } else {
   for (var i = 0; i < splword.length; i++) {
     var letter    = document.getElementById('letter'+(i+1)+"row"+row)
     letter.innerHTML = splraad[i];
     if (splword[i] === splraad[i]) {
-      letter.style.backgroundColor = "red";
-      letter.style.borderRadius = "0px";
-    } else if (splword[i] != splraad[i]) {
-      if (woord.includes(splraad[i])) {
-        letter.style.borderRadius = "50px";
-        letter.style.backgroundColor = "yellow";
-      }else {
-        letter.style.backgroundColor = "blue";
-        letter.style.borderRadius = "0px";
+      kopiewoordSpl[i] = "#";
+      if (row != 5) {
+        document.getElementById('letter'+(i+1)+"row"+(row+1)).innerHTML = splraad[i];
+        document.getElementById('letter'+(i+1)+"row"+(row+1)).style.backgroundColor = "red";
+        document.getElementById('letter'+(i+1)+"row"+(row+1)).style.borderRadius = "0px";
       }
+    } else if (splword[i] != splraad[i]) {
+      kopiewoordSpl[i] = splword[i];
     }
 }
+checkG();
 }
-row++;
+}
+
+function checkG() {
+  var splword = woord.split("");
+  var splraad = theLetter.value.split("");
+
+  antwoord = kopiewoordSpl[0]+kopiewoordSpl[1]+kopiewoordSpl[2]+kopiewoordSpl[3]+kopiewoordSpl[4];
+
+  for (var i = 0; i < kopiewoordSpl.length; i++) {
+    var letter    = document.getElementById('letter'+(i+1)+"row"+row);
+    if (kopiewoordSpl[i] === "#") {
+      letter.style.backgroundColor = "red";
+      letter.style.borderRadius = "0px";
+    } else if (antwoord.includes(splraad[i])) {
+      letter.style.backgroundColor = "yellow";
+      letter.style.borderRadius = "25px";
+    }
+  }
+  theLetter.value = "";
+  rowCheck();
+}
+
+function rowCheck() {
+  if (row === 5) {
+    alert("de ronde is voorbij");
+    document.getElementById('opnieuw').style.display = "";
+  } else {
+
+    row++;
+  }
+}
+
+function opnieuw() {
+  newWoord();
+  kopieer();
+  row = 1;
+  for (var r = 1; r <=5 ; r++) {
+    for (var i = 1; i <=5 ; i++) {
+    var letter    = document.getElementById('letter'+(i)+"row"+r)
+    letter.innerHTML = "_";
+    letter.style.backgroundColor = "blue";
+    letter.style.borderRadius = "0px";
+   }
+  }
+  document.getElementById('opnieuw').style.display = "none";
 }
